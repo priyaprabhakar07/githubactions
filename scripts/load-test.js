38d7af562b42394a2823cc6cf79cb8a8555cb15d
Priya Prabhakar <priyaprabhakarfreelancer@gmail.com>
2024-08-21 17:16:08 +0530

    corrected port in loadtest

--- scripts/load-test.js
+++ scripts/load-test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export default function () {
-  const res = http.get('http://foo.localhost:5678');
+  const res = http.get('http://foo.localhost');
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
