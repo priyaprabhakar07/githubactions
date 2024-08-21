db172b1c8dece33bf3107d3a1b196871d8db5bc4
Priya Prabhakar <priyaprabhakarfreelancer@gmail.com>
2024-08-21 17:34:14 +0530

    added port forwarding

--- scripts/load-test.js
+++ scripts/load-test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export default function () {
-  const res = http.get('http://foo.localhost');
+  const res = http.get('http://foo.localhost:8080');
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
