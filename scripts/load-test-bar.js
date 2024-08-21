eefd3be32f93935376408e45e98d1ce5d7ec3ad2
Priya Prabhakar <priyaprabhakarfreelancer@gmail.com>
2024-08-21 22:01:25 +0530

    updated the port for bar service test

--- scripts/load-test-bar.js
+++ scripts/load-test-bar.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export default function () {
-  const res = http.get('http://bar.localhost:8080');
+  const res = http.get('http://bar.localhost:8081');
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
