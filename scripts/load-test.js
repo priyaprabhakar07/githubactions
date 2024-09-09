c71a9b9405119dd79e3339509d4ab4c1bf5bc9f6
Priya Prabhakar <priyaprabhakarfreelancer@gmail.com>
2024-09-09 18:35:43 +0530

    added load balancing logic to test whole syste,

--- scripts/load-test.js
+++ scripts/load-test.js
@@ -1,10 +1,21 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+// Store the server list in a global variable
+const servers = [
+  'http://bar.localhost:8081',
+  'http://foo.localhost:8080',
+];
+
+let currentServerIndex = 0;
+
 export default function () {
-  const res = http.get('http://foo.localhost:8080');
-  check(res, {
-    'status is 200': (r) => r.status === 200,
-  });
-  sleep(1);
+  // Cycle through the servers
+  const url = servers[currentServerIndex];
+  currentServerIndex = (currentServerIndex + 1) % servers.length;
+
+  const res = http.get(url);
+  check(res, { 'is status 200': (r) => r.status === 200 });
+  
+  sleep(1); // Adjust the sleep time as needed
 }
